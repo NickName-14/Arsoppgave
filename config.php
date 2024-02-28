e42ad497a383513ea6ed4ef1c992b86442085219
NickName-14 <vikja001@outlook.com>
2024-02-28 09:55:49 +0100

    config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "10.200.1.108";
+$username = "root";
+$password = "Admin";
+$dbname = "mydb";
+
+// koble til databasen
+$link = new mysqli($servername, $username, $password, $dbname);
+
+// sjekke om det fungerte
+if ($link->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
