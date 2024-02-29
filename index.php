d29d392df871c2e1e5506ec2a8eee35404abbe32
NickName-14 <vikja001@outlook.com>
2024-02-29 11:11:27 +0100

    Add icon

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ require_once "config.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hjem</title>
+    <title>Hjem-Digistore</title>
+    <link rel="icon" type="image/x-icon" href="Bilder/IconAarsoppgave.png">
 </head>
 <body>
     <h1>En Nettbutikk</h1>
