babf64128534332b7eb27c446c4b2405880b67cc
NickName-14 <vikja001@outlook.com>
2024-03-11 10:03:55 +0100

    test

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once "config.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hjem-Digistore</title>
-    <link rel="icon" type="image/x-icon" href="Bilder/IconAarsoppgave.png">
+    <link rel="icon" type="image/x-icon" href="Bilder/Logo/IconAarsoppgave.png">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
